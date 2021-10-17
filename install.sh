982fcd255aa1ffed3948978afca91ab268187477
Nathan Perron <natou022@gmail.com>
2021-10-16 23:45:16 -0400

    Show nothing but custom messages

--- install.sh
+++ install.sh
@@ -1,22 +1,23 @@
 # Install packages wanted and needed
 echo "Installing packages" ;
-sudo apt-get install -y git make cmake curl zip ranger exa autojump nitrogen arandr psmisc dunst libnotify-bin lxappearance qt5ct qt5-style-kvantum nautilus flameshot rxvt-unicode volumeicon-alsa blueman network-manager-gnome neofetch polybar rofi gdm3 ; 
+sudo apt-get install -y git make cmake curl zip ranger exa autojump nitrogen arandr psmisc dunst libnotify-bin lxappearance qt5ct qt5-style-kvantum nautilus flameshot rxvt-unicode volumeicon-alsa blueman network-manager-gnome neofetch polybar rofi gdm3 > /dev/null ;
 
 # Install dependencies for picom
 # https://github.com/ibhagwan/picom
 echo "Installing packages for picom" ;
-sudo apt-get install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev ;
+sudo apt-get install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev > /dev/null ;
 
 # Install dependecies for i3-gaps
 # https://github.com/Airblader/i3
 echo "Installing packages for i3-gaps" ;
-sudo apt-get install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ;
+sudo apt-get install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev > /dev/null ;
 
 # Create missing directories
 [ ! -d "$HOME/.config" ] && mkdir "$HOME/.config" ;
 [ ! -d "$HOME/.config/gtk-2.0" ] && mkdir "$HOME/.config/gtk-2.0" ;
 [ ! -d "$HOME/.local" ] && mkdir "$HOME/.local" ;
 [ ! -d "$HOME/.local/share" ] && mkdir "$HOME/.local/share" ;
+[ ! -d "$HOME/.local/share/applications" ] && mkdir "$HOME/.local/share/applications" ;
 [ ! -d "$HOME/.local/bin" ] && mkdir "$HOME/.local/bin" ;
 [ ! -d "$HOME/.themes" ] && mkdir "$HOME/.themes" ;
 [ ! -d "$HOME/.icons" ] && mkdir "$HOME/.icons" ;
@@ -27,13 +28,13 @@ sudo apt-get install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev 
 
 # Build picom from source
 echo "Building picom" ;
-git clone https://github.com/ibhagwan/picom.git && 
+git clone https://github.com/ibhagwan/picom.git > /dev/null && 
 cd picom &&
-git submodule update --init --recursive &&
-meson --buildtype=release . build &&
-ninja -C build &&
+git submodule update --init --recursive > /dev/null &&
+meson --buildtype=release . build >/dev/null &&
+ninja -C build > /dev/null &&
 cd build &&
-sudo ninja install ; 
+sudo ninja install > /dev/null; 
 
 # Cleanup
 cd ~/installing && 
@@ -41,13 +42,13 @@ rm -rf picom;
 
 # Build i3-gaps from source
 echo "Building i3-gaps" ;
-git clone https://github.com/Airblader/i3.git i3-gaps && 
+git clone https://github.com/Airblader/i3.git i3-gaps > /dev/null && 
 cd i3-gaps &&
-mkdir -p build && 
+mkdir -p build > /dev/null && 
 cd build && 
-meson --prefix /usr/local && 
-ninja && 
-sudo ninja install ; 
+meson --prefix /usr/local > /dev/null && 
+ninja > /dev/null && 
+sudo ninja install > /dev/null ; 
 [ ! -f "/usr/share/xsessions/i3.desktop" ] && 
 touch "/usr/share/xsessions/i3.desktop" ;
 cat > /usr/share/xsessions/i3.desktop
@@ -69,13 +70,15 @@ rm -rf i3-gaps ;
 
 # Installing firefox developer edition
 echo "Installing firefox developer edition" ;
-curl -L0 https://download.mozilla.org/?product=firefox-develedition-latest-ssl&os=linux&lang=en-CA --output firefox.tar.bz2 &&
+curl -L0 https://download.mozilla.org/?product=firefox-develedition-latest-ssl&os=linux&lang=en-CA --output firefox.tar.bz2 > /dev/null &&
 sudo mv firefox.tar.bz2 /opt &&
 cd /opt &&
-sudo tar xfj firefox.tar.bz2 &&
+sudo tar xfj firefox.tar.bz2 > /dev/null &&
 sudo rm -rf firefox.tar.bz2 &&
 sudo chown -R $USER:$USER /opt/firefox && 
 cd ~/installing ;
+[ ! -f "$HOME/.local/share/applications/firefoxDeveloperEdition.desktop" ] &&
+touch "$HOME/.local/share/applications/firefoxDeveloperEdition.desktop" ;
 cat > ~/.local/share/applications/firefoxDeveloperEdition.desktop
 <<EOL
 [Desktop Entry]
@@ -97,28 +100,28 @@ EOL
 # Install Dracula theme for GTK
 # https://github.com/matheuuus/dracula-theme
 echo "Installing dracula theme" ;
-curl -L0 https://github.com/matheuuus/dracula-theme/archive/refs/heads/master.zip --output Dracula.zip &&
-unzip Dracula.zip &&
+curl -L0 https://github.com/matheuuus/dracula-theme/archive/refs/heads/master.zip --output Dracula.zip > /dev/null &&
+unzip Dracula.zip > /dev/null &&
 mv dracula-theme-master ~/.themes/Dracula && 
 rm -rf Dracula.zip ;
 
 # Install Dracula icons
 # https://github.com/matheuuus/dracula-icons
 echo "Installing dracula icons" ;
-curl -L0 https://github.com/matheuuus/dracula-icons/archive/refs/heads/main.zip --output Dracula-icons.zip && 
-unzip Dracula-icons.zip &&
+curl -L0 https://github.com/matheuuus/dracula-icons/archive/refs/heads/main.zip --output Dracula-icons.zip > /dev/null && 
+unzip Dracula-icons.zip > /dev/null &&
 mv dracula-icons-main ~/.icons/Dracula && 
 rm -rf Dracula-icons.zip ;
 
 # Install fonts
 # IPAExGothic (Japanese)
 echo "Installing Japanese font" ;
-sudo apt-get install -y fonts-ipaexfont-gothic ;
+sudo apt-get install -y fonts-ipaexfont-gothic > /dev/null;
 
 # Font Awesome
 echo "Installing Font-Awesome font"
-curl -L0 https://github.com/FortAwesome/Font-Awesome/archive/refs/heads/master.zip --output Font-Awesome.zip &&
-unzip Font-Awesome.zip && 
+curl -L0 https://github.com/FortAwesome/Font-Awesome/archive/refs/heads/master.zip --output Font-Awesome.zip > /dev/null &&
+unzip Font-Awesome.zip > /dev/null && 
 mv Font-Awesome-master/webfonts/*.ttf ~/.local/share/fonts/ &&
 mv Font-Awesome-master/otfs/*.otf ~/.local/share/fonts/ &&
 rm -rf Font-Awesome.zip &&
@@ -126,8 +129,8 @@ rm -rf Font-Awesome-master ;
 
 # Nerd Font
 echo "Installing Nerd font" ;
-curl -L0 https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Overpass.zip --output NerdFonts.zip &&
-unzip NerdFonts.zip &&
+curl -L0 https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Overpass.zip --output NerdFonts.zip > /dev/null &&
+unzip NerdFonts.zip > /dev/null &&
 mv *.otf ~/.local/share/fonts/ && 
 rm -rf NerdFonts.zip;
 
@@ -150,7 +153,7 @@ echo "Installing personal dotfiles" ;
 [ -f "$HOME/.config/user-dirs.dirs" ] && rm -rf "$HOME/.config/user-dirs.dirs" ;
 [ -f "$HOME/.bashrc" ] && rm -rf "$HOME/.bashrc" ;
 [ -f "$HOME/.profile" ] && rm -rf "$HOME/.profile" ;
-git clone https://natperron@github.com/natperron/dotfiles.git &&
+git clone https://natperron@github.com/natperron/dotfiles.git > /dev/null &&
 mv dotfiles/.config/* ~/.config/ && 
 mv dotfiles/.bashrc ~/.bashrc && 
 mv dotfiles/.profile ~/.profile && 

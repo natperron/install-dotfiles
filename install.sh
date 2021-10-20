0c5a53cd5c668dedb8b59bf8657e71ff7a92c3f8
Nathan Perron <natou022@gmail.com>
2021-10-20 00:16:18 -0400

    Silence most

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 # Install packages wanted and needed
 echo "Installing packages" ;
-sudo apt-get install -y git make cmake curl zip ranger exa autojump nitrogen arandr psmisc dunst libnotify-bin lxappearance qt5ct qt5-style-kvantum nautilus flameshot rxvt-unicode volumeicon-alsa blueman network-manager-gnome neofetch polybar rofi gdm3 papirus-icon-theme numix-icon-theme-circle ;
+sudo apt-get install -y -q=2 git make cmake curl zip ranger exa autojump nitrogen arandr psmisc dunst libnotify-bin lxappearance qt5ct qt5-style-kvantum nautilus flameshot rxvt-unicode volumeicon-alsa blueman network-manager-gnome neofetch polybar rofi gdm3 papirus-icon-theme numix-icon-theme-circle ;
 
 # Install dependencies for picom
 # https://github.com/ibhagwan/picom
 echo "Installing packages for picom" ;
-sudo apt-get install -y libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev ;
+sudo apt-get install -y -q=2 libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev ;
 
 # Install dependecies for i3-gaps
 # https://github.com/Airblader/i3
 echo "Installing packages for i3-gaps" ;
-sudo apt-get install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ;
+sudo apt-get install -y -q=2 meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev ;
 
 # Create missing directories
 [ ! -d "$HOME/.config" ] && mkdir "$HOME/.config" ;
@@ -29,7 +29,7 @@ sudo apt-get install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev 
 
 # Build picom from source
 echo "Building picom" ;
-git clone https://github.com/ibhagwan/picom.git && 
+git clone -q https://github.com/ibhagwan/picom.git && 
 cd picom &&
 git submodule update --init --recursive &&
 meson --buildtype=release . build &&
@@ -43,17 +43,16 @@ rm -rf picom;
 
 # Build i3-gaps from source
 echo "Building i3-gaps" ;
-git clone https://github.com/Airblader/i3.git i3-gaps && 
+git clone -q https://github.com/Airblader/i3.git i3-gaps && 
 cd i3-gaps &&
 mkdir -p build && 
 cd build && 
 meson --prefix /usr/local && 
 ninja && 
 sudo ninja install ; 
-[ ! -f "/usr/share/xsessions/i3.desktop" ] && 
-touch "/usr/share/xsessions/i3.desktop" ;
-cat > /usr/share/xsessions/i3.desktop
-<<EOL
+[ ! -f "i3.desktop" ] && 
+touch "i3.desktop" ;
+cat > i3.desktop <<EOL
 [Desktop Entry]
 Name=i3
 Comment=improved dynamic tiling window manager
@@ -65,13 +64,15 @@ DesktopNames=i3
 Keywords=tiling;wm;windowmanager;window;manager;
 EOL
 
+sudo mv i3.desktop /usr/share/xsessions ;
+
 # Cleanup
 cd ~/installing && 
 rm -rf i3-gaps ;
 
 # Installing firefox developer edition
 echo "Installing firefox developer edition" ;
-curl -L0 https://download.mozilla.org/?product=firefox-develedition-latest-ssl&os=linux&lang=en-CA --output firefox.tar.bz2 &&
+curl -s -L0 "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-CA" --output firefox.tar.bz2 &&
 sudo mv firefox.tar.bz2 /opt &&
 cd /opt &&
 sudo tar xfj firefox.tar.bz2 &&
@@ -80,8 +81,7 @@ sudo chown -R $USER:$USER /opt/firefox &&
 cd ~/installing ;
 [ ! -f "$HOME/.local/share/applications/firefoxDeveloperEdition.desktop" ] &&
 touch "$HOME/.local/share/applications/firefoxDeveloperEdition.desktop" ;
-cat > ~/.local/share/applications/firefoxDeveloperEdition.desktop
-<<EOL
+cat > ~/.local/share/applications/firefoxDeveloperEdition.desktop <<EOL
 [Desktop Entry]
 Encoding=UTF-8
 Name=Firefox Developer Edition
@@ -101,16 +101,16 @@ EOL
 # Install Dracula theme for GTK
 # https://github.com/matheuuus/dracula-theme
 echo "Installing dracula theme" ;
-curl -L0 https://github.com/matheuuus/dracula-theme/archive/refs/heads/master.zip --output Dracula.zip &&
-unzip Dracula.zip &&
+curl -s -L0 https://github.com/matheuuus/dracula-theme/archive/refs/heads/master.zip --output Dracula.zip &&
+unzip -qq Dracula.zip &&
 mv dracula-theme-master ~/.themes/Dracula && 
 rm -rf Dracula.zip ;
 
 # Install Dracula icons
 # https://github.com/matheuuus/dracula-icons
 echo "Installing dracula icons" ;
-curl -L0 https://github.com/matheuuus/dracula-icons/archive/refs/heads/main.zip --output Dracula-icons.zip && 
-unzip Dracula-icons.zip &&
+curl -s -L0 https://github.com/matheuuus/dracula-icons/archive/refs/heads/main.zip --output Dracula-icons.zip && 
+unzip -qq Dracula-icons.zip &&
 mv dracula-icons-main ~/.icons/Dracula && 
 rm -rf Dracula-icons.zip ;
 # Fix icons
@@ -121,12 +121,12 @@ cp "/usr/share/icons/hicolor/scalable/apps/lutris.svg" "$HOME/.icons/Dracula/sca
 # Install fonts
 # IPAExGothic (Japanese)
 echo "Installing Japanese font" ;
-sudo apt-get install -y fonts-ipaexfont-gothic;
+sudo apt-get install -y -q=2 fonts-ipaexfont-gothic;
 
 # Font Awesome
 echo "Installing Font-Awesome font"
-curl -L0 https://github.com/FortAwesome/Font-Awesome/archive/refs/heads/master.zip --output Font-Awesome.zip &&
-unzip Font-Awesome.zip && 
+curl -s -L0 https://github.com/FortAwesome/Font-Awesome/archive/refs/heads/master.zip --output Font-Awesome.zip &&
+unzip -qq Font-Awesome.zip && 
 mv Font-Awesome-master/webfonts/*.ttf ~/.local/share/fonts/ &&
 mv Font-Awesome-master/otfs/*.otf ~/.local/share/fonts/ &&
 rm -rf Font-Awesome.zip &&
@@ -134,8 +134,8 @@ rm -rf Font-Awesome-master ;
 
 # Nerd Font
 echo "Installing Nerd font" ;
-curl -L0 https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Overpass.zip --output NerdFonts.zip &&
-unzip NerdFonts.zip &&
+curl -s -L0 https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Overpass.zip --output NerdFonts.zip &&
+unzip -qq NerdFonts.zip &&
 mv *.otf ~/.local/share/fonts/ && 
 rm -rf NerdFonts.zip;
 
@@ -158,7 +158,7 @@ echo "Installing personal dotfiles" ;
 [ -f "$HOME/.config/user-dirs.dirs" ] && rm -rf "$HOME/.config/user-dirs.dirs" ;
 [ -f "$HOME/.bashrc" ] && rm -rf "$HOME/.bashrc" ;
 [ -f "$HOME/.profile" ] && rm -rf "$HOME/.profile" ;
-git clone https://natperron@github.com/natperron/dotfiles.git &&
+git clone -q https://natperron@github.com/natperron/dotfiles.git &&
 mv dotfiles/.config/* ~/.config/ && 
 mv dotfiles/.bashrc ~/.bashrc && 
 mv dotfiles/.profile ~/.profile && 
